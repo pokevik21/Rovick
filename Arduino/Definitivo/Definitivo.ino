cd5dd0f8b3876cbb2564c4505e4293ad398fe832
Unknown <pokevik21@gmail.com>
2018-08-28 21:56:00 +0200

    10/12 moves

--- Arduino/Definitivo/Definitivo.ino
+++ Arduino/Definitivo/Definitivo.ino
@@ -38,7 +38,7 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define upDesAgarrado 440
 
 //ABAJO
-#define pinBajoBr 4
+#define pinBajoBr 2
 #define pinBajoAg 10
 #define bajoR 85
 #define bajoMid 276
@@ -46,31 +46,45 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define bajoAgarrado 290
 #define bajoDesAgarrado 460
 
+//otras
+char inChar;
+String toDo="";
+boolean TransmisionCompleta=false;
 
 /******************************************** FIN VARIABLES ***************************************************/
 
 
 void posIni(){
-  doMove(pinBajoAg,bajoAgarrado);
-  doMove(pinUpAg,upAgarrado);
-  doMove(pinDerBr,derMid);
-  doMove(pinDerAg,derAgarrado);
-  doMove(pinIzqBr,izqMid);
-  doMove(pinIzqAg,izqAgarrado);
+  pwm.setPWM(pinDerBr, 0, derMid);
+  pwm.setPWM(pinIzqBr, 0, izqMid);
+  pwm.setPWM(pinUpBr, 0, upMid);
+  pwm.setPWM(pinBajoBr, 0, bajoMid);
+  delay(1000);
+
+  sameTime(pinBajoAg,pinUpAg,bajoAgarrado,upAgarrado);
+  sameTime(pinDerAg,pinIzqAg,derAgarrado,izqAgarrado);
 }
 
 void fin(){
-  doMove(pinDerAg,derDesAgarrado);
+  pwm.setPWM(pinDerBr, 0, derMid);
+  pwm.setPWM(pinIzqBr, 0, izqMid);
+  pwm.setPWM(pinUpBr, 0, upMid);
+  pwm.setPWM(pinBajoBr, 0, bajoMid);
+  delay(1000);
+  
+
   doMove(pinUpAg,upDesAgarrado);
-  doMove(pinIzqAg,izqDesAgarrado);
+  sameTime(pinDerAg,pinIzqAg,derDesAgarrado,izqDesAgarrado);
+  doMove(pinBajoAg,bajoDesAgarrado);
 }
 
 void setup()
 {
   Serial.begin(9600);
   pwm.begin();
+  toDo.reserve(100);
   pwm.setPWMFreq(FREQUENCY);
-  //posIniciales();
+  posIni();
   Serial.println("Listo!");
 }
 /******************************************** FIN INICIO ***************************************************/
@@ -86,6 +100,11 @@ void doMove(int pin,int posicion){
   delay(500);
 }
 
+void sameTime(int pin1, int pin2 , int pos1, int pos2){
+  pwm.setPWM(pin1, 0, pos1);
+  pwm.setPWM(pin2, 0, pos2);
+  delay(500);
+}
 
 //      ____                                _             
 //     |  _ \    ___   _ __    ___    ___  | |__     __ _ 
@@ -169,9 +188,108 @@ void D(){
   doMove(pinBajoAg,bajoAgarrado);
 }
 
+//      _____                          _             _ 
+//     |  ___|  _ __    ___    _ __   | |_    __ _  | |
+//     | |_    | '__|  / _ \  | '_ \  | __|  / _` | | |
+//     |  _|   | |    | (_) | | | | | | |_  | (_| | | |
+//     |_|     |_|     \___/  |_| |_|  \__|  \__,_| |_|
+
+void Fo(){
+  sameTime(pinDerAg,pinIzqAg,derDesAgarrado,izqDesAgarrado);
+  sameTime(pinBajoBr,pinUpBr,bajoL,upR);
+  sameTime(pinDerAg,pinIzqAg,derAgarrado,izqAgarrado);
+  sameTime(pinUpAg,pinBajoAg,upDesAgarrado,bajoDesAgarrado);
+  sameTime(pinUpBr,pinBajoBr,upMid,bajoMid);
+  sameTime(pinUpAg,pinBajoAg,upAgarrado,bajoAgarrado);
+  doMove(pinDerBr,derUp);
+  doMove(pinDerAg,derDesAgarrado);
+  doMove(pinDerBr,derMid);
+  doMove(pinIzqAg,izqDesAgarrado);
+  sameTime(pinBajoBr,pinUpBr,bajoR,upL);
+  sameTime(pinDerAg,pinIzqAg,derAgarrado,izqAgarrado);
+  sameTime(pinUpAg,pinBajoAg,upDesAgarrado,bajoDesAgarrado);
+  sameTime(pinUpBr,pinBajoBr,upMid,bajoMid);
+  sameTime(pinUpAg,pinBajoAg,upAgarrado,bajoAgarrado);
+}
+
+
+void FD(){
+  sameTime(pinDerAg,pinIzqAg,derDesAgarrado,izqDesAgarrado);
+  sameTime(pinBajoBr,pinUpBr,bajoL,upR);
+  sameTime(pinDerAg,pinIzqAg,derAgarrado,izqAgarrado);
+  sameTime(pinUpAg,pinBajoAg,upDesAgarrado,bajoDesAgarrado);
+  sameTime(pinUpBr,pinBajoBr,upMid,bajoMid);
+  sameTime(pinUpAg,pinBajoAg,upAgarrado,bajoAgarrado);
+  doMove(pinDerBr,derDown);
+  doMove(pinDerAg,derDesAgarrado);
+  doMove(pinDerBr,derMid);
+  doMove(pinIzqAg,izqDesAgarrado);
+  sameTime(pinBajoBr,pinUpBr,bajoR,upL);
+  sameTime(pinDerAg,pinIzqAg,derAgarrado,izqAgarrado);
+  sameTime(pinUpAg,pinBajoAg,upDesAgarrado,bajoDesAgarrado);
+  sameTime(pinUpBr,pinBajoBr,upMid,bajoMid);
+  sameTime(pinUpAg,pinBajoAg,upAgarrado,bajoAgarrado);
+}
+
+void FIda(){
+  sameTime(pinDerAg,pinIzqAg,derDesAgarrado,izqDesAgarrado);
+  sameTime(pinBajoBr,pinUpBr,bajoL,upR);
+  sameTime(pinDerAg,pinIzqAg,derAgarrado,izqAgarrado);
+  sameTime(pinUpAg,pinBajoAg,upDesAgarrado,bajoDesAgarrado);
+  sameTime(pinUpBr,pinBajoBr,upMid,bajoMid);
+  sameTime(pinUpAg,pinBajoAg,upAgarrado,bajoAgarrado);
+  doMove(pinDerBr,derUp);
+  doMove(pinDerAg,derDesAgarrado);
+  doMove(pinDerBr,derMid);
+  doMove(pinDerAg,derAgarrado);
+}
+
+void FDIda(){
+  sameTime(pinDerAg,pinIzqAg,derDesAgarrado,izqDesAgarrado);
+  sameTime(pinBajoBr,pinUpBr,bajoL,upR);
+  sameTime(pinDerAg,pinIzqAg,derAgarrado,izqAgarrado);
+  sameTime(pinUpAg,pinBajoAg,upDesAgarrado,bajoDesAgarrado);
+  sameTime(pinUpBr,pinBajoBr,upMid,bajoMid);
+  sameTime(pinUpAg,pinBajoAg,upAgarrado,bajoAgarrado);
+  doMove(pinDerBr,derDown);
+  doMove(pinDerAg,derDesAgarrado);
+  doMove(pinDerBr,derMid);
+  doMove(pinDerAg,derAgarrado);
+}
+
+void FVuelta(){
+  sameTime(pinDerAg,pinIzqAg,derDesAgarrado,izqDesAgarrado);
+  sameTime(pinBajoBr,pinUpBr,bajoR,upL);
+  sameTime(pinDerAg,pinIzqAg,derAgarrado,izqAgarrado);
+  sameTime(pinUpAg,pinBajoAg,upDesAgarrado,bajoDesAgarrado);
+  sameTime(pinUpBr,pinBajoBr,upMid,bajoMid);
+  sameTime(pinUpAg,pinBajoAg,upAgarrado,bajoAgarrado);
+}
+
+//      ____                   _    
+//     | __ )    __ _    ___  | | __
+//     |  _ \   / _` |  / __| | |/ /
+//     | |_) | | (_| | | (__  |   < 
+//     |____/   \__,_|  \___| |_|\_\
 
 
 /******************************************** FIN METODOS ***************************************************/
+
+void serialEvent() {
+  while (Serial.available()) {
+
+    char CharEntrada = Serial.read(); //Leer un byte del puerto serial
+
+    toDo += CharEntrada;  //Agregar el char anterior al string
+
+    if (CharEntrada == ';') {  //Si se detecta un fin de linea
+      size_t len = toDo.length();
+      toDo.remove(len-1);
+      TransmisionCompleta = true;  //Se indica al programa que el usuario termino de ingresar la informacion
+    }
+  }
+}
+
 
 //     _                             
 //    | |       ___     ___    _ __  
@@ -182,10 +300,81 @@ void D(){
 
 void loop() {
 
-  while (Serial.available() == 0){ }
-  int ang = Serial.parseInt();
+   if (TransmisionCompleta) {
+    char first = toDo.charAt(0);
+    if(toDo.length() == 1){
+        //solo acciones con un caracter
+      switch(first){
+        case 'R':
+            R();
+          break;
+        case 'L':
+            L();
+          break;
+        case 'U':
+            U();
+          break;
+        case 'D':
+            D();
+          break;
+       case 'F':
+            Fo();
+          break;
+       case 'B':
+            
+          break;
+       case 'I':
+            FIda();
+          break;
+       case 'V':
+            FVuelta();
+          break;     
+      }
+      
+    }else if(toDo.length() > 1){
+
+      switch(first){
+        case 'R':
+            RD();
+          break;
+        case 'L':
+            LD();
+          break;
+        case 'U':
+            UD();
+          break;
+        case 'D':
+            DD();
+          break;
+       case 'F':
+            FD();
+          break;
+       case 'B':
+            
+          break;   
+       case 'S':
+            posIni();
+          break;
+       case 'E':
+            fin();
+          break; 
+       case 'I':
+            FDIda();
+          break;    
+       case 'V':
+            FVuelta();
+          break;  
+      }
+      
+    }
+
+    
+    
+    toDo = "";  //Limpiar el String
+    TransmisionCompleta = false;  //Limpiar la bandera
+}
 
   
   
-  Serial.println("terminado");
+
 }
