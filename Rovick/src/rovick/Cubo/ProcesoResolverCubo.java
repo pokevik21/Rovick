41d65be2ff423ccc87a252ad6cf7ceb288209bf6
pokevik21 <pokevik21@gmail.com>
2018-11-19 23:58:51 +0100

    casi_color

--- Rovick/src/rovick/Cubo/ProcesoResolverCubo.java
+++ Rovick/src/rovick/Cubo/ProcesoResolverCubo.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -32,7 +33,7 @@ public class ProcesoResolverCubo extends Thread{
     private int[] c4 = null;
     private int[] c5 = null;*/
     
-    private Color centros[];
+    private ArrayList<float[]> centros;
 
     private BufferedImage image_1 = null;
     private BufferedImage image_2 = null;
@@ -62,7 +63,7 @@ public class ProcesoResolverCubo extends Thread{
             if(!foto.isEmpty())camara.takePhoto(foto);
     }
     
-    private boolean inColor(float h, Color c){
+    /*private boolean inColor(float h, Color c){
         int R = c.getRed();
         int G = c.getGreen();
         int B = c.getRed();
@@ -70,45 +71,48 @@ public class ProcesoResolverCubo extends Thread{
         Color.RGBtoHSB(R, G, B, hsv);
         System.out.println("**************************************");
         System.out.println("R:"+R+" ;G:"+G + "  ;B:"+B);
-        System.out.println("**************************************");
+        System.out.println("**************************************");*/
         
        /* if(   (R >= (r[0]) && R<=(r[3]))
            && (G >= (r[1]) && G<=(r[4]))  
            && (B >= (r[2]) && B<=(r[5]))){
             return true;
         } */
-        return false;
-    }
+        /*return false;
+    }*/
     
     /*private boolean intensidadParecidas(float h1, float h2 ,int rango){
         return (h1>=h2-rango) && (h1<=h2+rango);
     }*/
     
-    private float distancia(Color c1, Color c2) {
-        float dR = (float) ((c1.getRed()-c2.getRed())/255.0);
-        float dG = (float) ((c1.getGreen()-c2.getGreen())/255.0);
-        float dB = (float) ((c1.getBlue()-c2.getBlue())/255.0);
-        float resultado = (float) Math.pow(2*Math.pow(dR, 2) + 4*Math.pow(dG, 2) + 3*Math.pow(dB, 2) , 1/2);
-        
+    private float distancia(float[] c1, float[] c2) {
+        float dR = (float) (c1[0]-c2[0]);
+        //System.out.println("red:"+dR);
+        float dG = (float) (c1[1]-c2[1]);
+        //System.out.println("green:"+dG);
+        float dB = (float) (c1[2]-c2[2]);
+        //System.out.println("blue:"+dB);
+        float resultado = (float) Math.sqrt(2*Math.pow(dR, 2) + 4*Math.pow(dG, 2) + 3*Math.pow(dB, 2)); 
+        //System.out.println("resultado:"+ resultado);
         return resultado;
     }
     
     //2*dR*dR + 4*dG*dG + 3*dB*
     private int sacarColor(BufferedImage image, int pos_x, int pos_y){
-        Color color = avg(image, pos_x, pos_y, 10);
+        float[] color = avg(image, pos_x, pos_y, 10);
         
         int mejor_color = 0;
         float mejor_distancia = 99999;
         for (int i = 0; i < 6; i++) {
             
-            float d = distancia(centros[i], color);
+            float d = distancia(centros.get(i), color);
             if (d < mejor_distancia) {
                 mejor_distancia = d;
                 mejor_color = i;
             }
         }
         
-        System.out.println("intesidad:"+color +"  ; mejor_distancia:"+mejor_distancia  +"  mejor:"+mejor_color);
+       // System.out.println("intesidad:"+Arrays.toString(color) +"  ; mejor_distancia:"+mejor_distancia  +"  mejor:"+mejor_color);
         
     return mejor_color;    
     }   
@@ -191,7 +195,9 @@ public class ProcesoResolverCubo extends Thread{
     }*/
     
     
-    private Color avg(BufferedImage image, int coord_x , int coord_y, int tam){
+    private float[] avg(BufferedImage image, int coord_x , int coord_y, int tam){
+        
+        float[] RGB = new float[3];
         
         float totalR = 0;
         float totalG = 0;
@@ -201,17 +207,17 @@ public class ProcesoResolverCubo extends Thread{
             for (int j = 0; j < tam; j++) {
                 Color c_pos = new Color(image.getRGB(coord_x + j, coord_y +i));
                 
-                totalR += c_pos.getRed()/255.0;
-                totalG += c_pos.getGreen()/255.0;
-                totalB += c_pos.getBlue()/255.0;
+                totalR += c_pos.getRed();
+                totalG += c_pos.getGreen();
+                totalB += c_pos.getBlue();
             }
         }
         
-        float avgR= (float)(totalR/Math.pow(tam, 2));
-        float avgG= (float)(totalG/Math.pow(tam, 2));
-        float avgB= (float)(totalB/Math.pow(tam, 2));
+        RGB[0] = (float)(totalR/Math.pow(tam, 2));
+        RGB[1] = (float)(totalG/Math.pow(tam, 2));
+        RGB[2] = (float)(totalB/Math.pow(tam, 2));
 
-     return new Color(avgR,avgG,avgB);
+     return RGB;
     }
     
     @Override
@@ -233,18 +239,26 @@ public class ProcesoResolverCubo extends Thread{
             image_5_2 = ImageIO.read(ImageIO.createImageInputStream(new FileInputStream("./test_images/5_2.png")));
             image_6 = ImageIO.read(ImageIO.createImageInputStream(new FileInputStream("./test_images/6.png")));
             
-            centros = new Color[6];
+            centros = new ArrayList<>();
             
-            centros[0] = avg(image_1,237 ,250,10);
-            centros[1] = avg(image_2,237 ,250,10);
-            centros[2] = avg(image_3_1,237 ,250,10);
-            centros[3] = avg(image_4,237 ,250,10);
-            centros[4] = avg(image_5_1,237 ,250,10);
-            centros[5] = avg(image_6,237 ,250,10);
+            centros.add(avg(image_1,237 ,250,10));
+            centros.add(avg(image_2,237 ,250,10));
+            centros.add(avg(image_3_1,237 ,250,10));
+            centros.add(avg(image_4,237 ,250,10));
+            centros.add(avg(image_5_1,237 ,250,10));
+            centros.add(avg(image_6,237 ,250,10));
 
-            int[] top = analizarCara(image_2);
-            System.out.println("centros:"+Arrays.toString(centros));
-            System.out.println(Arrays.toString(top));
+            int[][] cube = new int[6][9];
+            cube[0]=analizarCara(image_1);
+            cube[1]=analizarCara(image_2);
+            cube[2]=analizarCara(image_3_1,image_3_2);
+            cube[3]=analizarCara(image_4);
+            cube[4]=analizarCara(image_5_1,image_5_2);
+            cube[5]=analizarCara(image_6);
+            
+            for (int[] is : cube) {
+                System.out.println(Arrays.toString(is));
+            }
             
         } catch (IOException ex) {
             Logger.getLogger(ProcesoResolverCubo.class.getName()).log(Level.SEVERE, null, ex);
