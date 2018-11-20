81db56affc7359397981b94c295759f73104dd9c
Unknown <pokevik21@gmail.com>
2018-11-20 09:41:32 +0100

    cambio_imagesTest

--- Rovick/src/rovick/Utils/PantallaDeCarga.java
+++ Rovick/src/rovick/Utils/PantallaDeCarga.java
@@ -101,7 +101,6 @@ public class PantallaDeCarga extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new PantallaDeCarga().setVisible(true);
-                
             }
         });
     }
